f55dd6b3b49c84fb2ed77d0aa97c47b7ccd2eb76
asymiii <iliadenisyuk333@gmail.com>
2025-04-18 18:09:41 +0300

    Update Денисюк.cpp

--- Денисюк.cpp
+++ Денисюк.cpp
@@ -1,27 +1,30 @@
 #include <iostream> // Чтобы можно было выводить на экран (cout) и вводить с клавиатуры (cin)
 #include <cmath>   // Чтобы можно было использовать математические функции, типа sin, cos, pow (возведение в степень)
+#define _use_math_defines
 
 using namespace std; // Чтобы не писать std:: перед cout, cin
+double getA(const double x, const double y);
+
+double getB(const double x, const double y);
+
 
 int main() {
     // ===== Исходные данные =====
-    double x = 0.335; // Объявляем переменную x типа double (число с плавающей точкой) и присваиваем ей значение 0.335
-    double y = 0.025; // То же самое для переменной y
-
-
-    // ===== Вычисление 'a' =====
-    // Объявляем переменную a типа double и вычисляем её значение по формуле
-    double a = 1 + x + (pow(x, 2) / 2.0) + (pow(x, 3) / 3.0) + (pow(x, 4) / 4.0);
-
-    // ===== Вычисление 'b' =====
-    // Объявляем переменную b типа double и вычисляем её значение по формуле
-    double b = x * (sin(pow(x, 3)) + pow(cos(y), 2));
-
+    const double x = 0.335; // Объявляем переменную x типа double (число с плавающей точкой) и присваиваем ей значение 0.335
+    const double y = 0.025; // То же самое для переменной y
     // ===== Вывод результатов вычислений =====
-    //cout << fixed << setprecision(6); // Устанавливаем точность вывода - 6 знаков после запятой
     cout << "Результаты вычислений:" << endl;
-    cout << "a = " << a << endl;  // Выводим значение a на экран
-    cout << "b = " << b << endl;  // Выводим значение b на экран
-
+    cout << "a = " << getA(x,y) << endl;  // Выводим значение a на экран
+    cout << "b = " << getB(x,y);  // Выводим значение b на экран
     return 0;
+}
+// ===== Вычисление 'a' =====
+double getA(const double x, const double y)
+{
+    return(1 + x + ((pow(x, 2)) / 2) + ((pow(x, 3)) / 3) + ((pow(x, 4)) / 4));
+}
+// ===== Вычисление 'b' =====
+double getB(const double x, const double y)
+{
+    return(x * (sin(pow(x, 3)) + pow(cos(y), 2)));
 }
