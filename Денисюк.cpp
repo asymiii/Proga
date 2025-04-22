2066bb9c6b9288e6cb95e4b176bb1bea820f2eb7
asymiii <iliadenisyuk333@gmail.com>
2025-04-22 21:31:40 +0300

    Update Денисюк.cpp

--- Денисюк.cpp
+++ Денисюк.cpp
@@ -1,29 +1,38 @@
-#include <iostream> // Чтобы можно было выводить на экран (cout) и вводить с клавиатуры (cin)
-#include <cmath>   // Чтобы можно было использовать математические функции, типа sin, cos, pow (возведение в степень)
-#define _use_math_defines
+#include <iostream> 
+#include <cmath>
+#define _use_math_defines //for c++
 
-using namespace std; // Чтобы не писать std:: перед cout, cin
+using namespace std;
+/**
+*@brief - Функция для вычисления А по заданному уравнению
+*@param х - значение переменной х
+*@param у - значение переменной у
+*@return- возвращает рассчитанное значение
+*/
 double getA(const double x, const double y);
-
+/**
+*@brief - Функция для вычисления А по заданному уравнению
+*@param х - значение переменной х
+*@param у - значение переменной у
+*@return- возвращает рассчитанное значение
+*/
 double getB(const double x, const double y);
-
-
+/**
+*@brief - Точко входа в программу
+*@return - возвращает 0, если программа выполнена корректно 
+*/
 int main() {
-    // ===== Исходные данные =====
-    const double x = 0.335; // Объявляем переменную x типа double (число с плавающей точкой) и присваиваем ей значение 0.335
-    const double y = 0.025; // То же самое для переменной y
-    // ===== Вывод результатов вычислений =====
+    const double x = 0.335; 
+    const double y = 0.025; 
     cout << "Результаты вычислений:" << endl;
-    cout << "a = " << getA(x,y) << endl;  // Выводим значение a на экран
-    cout << "b = " << getB(x,y);  // Выводим значение b на экран
+    cout << "a = " << getA(x,y) << endl; 
+    cout << "b = " << getB(x,y); 
     return 0;
 }
-// ===== Вычисление 'a' =====
 double getA(const double x, const double y)
 {
     return(1 + x + ((pow(x, 2)) / 2) + ((pow(x, 3)) / 3) + ((pow(x, 4)) / 4));
 }
-// ===== Вычисление 'b' =====
 double getB(const double x, const double y)
 {
     return(x * (sin(pow(x, 3)) + pow(cos(y), 2)));
