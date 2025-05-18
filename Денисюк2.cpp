65d5f9c86a32cba889b46d91c7ed71f8fbb82012
asymiii <iliadenisyuk333@gmail.com>
2025-05-18 23:11:08 +0300

    Update Денисюк2.cpp

--- Денисюк2.cpp
+++ Денисюк2.cpp
@@ -1,40 +1,62 @@
-#include <iostream>  // Для ввода/вывода данных (cin, cout)
-#include <cmath>    // Для математических функций (pow, PI)
-#include <limits>   // Чтобы очищать поток ввода, если пользователь ввел что-то не то
-#include<cstdlib>   // для модуля
+#include <iostream>
+#include <cmath>
+#include <limits> 
+
 using namespace std;
 
+/**
+ * @brief - Функция для вычисления объема шара
+ * @param radius - радиус шара
+ * @return - возвращает объем шара
+ */
+double calculateVolume(const double radius);
+
+/**
+ * @brief - Функция для вычисления площади поверхности шара
+ * @param radius - радиус шара
+ * @return - возвращает площадь поверхности шара
+ */
+double calculateSurfaceArea(const double radius);
+
+/**
+ * @brief - Проверяет, является ли ввод корректным числом
+ * @return - возвращает введенное число, если оно корректно
+ */
+double getValidatedInput();
+
+/**
+ * @brief - Точка входа в программу
+ * @return - возвращает 0, если программа выполнена корректно
+ */
 int main() {
-  double radius; // Объявляем переменную для хранения радиуса шара
-  double volume; // Объявляем переменную для хранения объема шара
-  double Area; // Объявляем переменную для хранения площади поверхности шара
-  const double PI = 3.14; // Объявляем константу PI (число пи)
+    cout << "Введите радиус шара R: ";
+    double radius = getValidatedInput();
 
-  // Запрашиваем ввод радиуса
-  cout << "Введите радиус шара: ";
+    cout << "Результаты вычислений:" << endl;
+    cout << "Объем шара: " << calculateVolume(radius) << endl;
+    cout << "Площадь поверхности шара: " << calculateSurfaceArea(radius) << endl;
 
-  // Проверка ввода на корректность (только числа)
-  while (!(cin >> (radius))) { // Пока ввод некорректен (не число)
-    cout << "Ошибка! Введите число: "; // Выводим сообщение об ошибке
-    cin.clear(); // Очищаем флаг ошибки у cin
-    cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Удаляем некорректный ввод из потока
-  }
+    return 0;
+}
 
+double calculateVolume(const double radius) {
+    return (4.0 / 3.0) * 3.14 * pow(radius, 3);
+}
 
-  // Формулы для расчета объема и площади поверхности шара:
-  // Объем шара: V = (4/3) * PI * R^3
-  // Площадь поверхности шара: S = 4 * PI * R^2
+double calculateSurfaceArea(const double radius) {
+    return 4 * 3.14 * pow(radius, 2);
+}
 
-  // Вычисляем объем шара
-  volume = (4.0 / 3.0) * PI * pow((abs(radius)), 3);
-  // Важно использовать 4.0 и 3.0 вместо 4 и 3, чтобы деление было вещественным
-
-  // Вычисляем площадь поверхности шара
-  Area = 4 * PI * pow((abs(radius)), 2);
-
-  // Выводим результаты на экран
-  cout << "Объем шара: " << volume << endl;
-  cout << "Площадь поверхности шара: " << Area << endl;
-
-  return 0; // Программа завершилась успешно
+double getValidatedInput() {
+    double input;
+    while (true) {
+        cin >> input;
+        if (cin.fail()) { // Если ввод не является числом
+            cin.clear(); // Сброс флага ошибки
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Очистка буфера ввода
+            cout << "Ошибка! Введите число: ";
+        } else {
+            return input;
+        }
+    }
 }
